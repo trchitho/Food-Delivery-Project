6b7b1e6d04e8b2a4ea518cc563c9af3b6e20a8b2
Tho <tranchitho160704@gmail.com>
2026-05-13 16:17:03 +0700

    feat: add JPA entities - Users, Roles, Restaurant, Food, Category, Orders, Promotion, Rating

---
+++ src/main/java/com/maxnguyen/fooddelivery/entity/Roles.java
@@ -0,0 +1,55 @@
+package com.maxnguyen.fooddelivery.entity;
+
+import jakarta.persistence.*;
+
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity(name = "roles")
+public class Roles {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "role_name")
+    private String roleName;
+    @Column(name = "description")
+    private String description;
+
+    @OneToMany(mappedBy = "roles")
+    private Set<Users> listUsers;
+
+    public Set<Users> getListUsers() {
+        return listUsers;
+    }
+
+    public void setListUsers(Set<Users> listUsers) {
+        this.listUsers = listUsers;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getRoleName() {
+        return roleName;
+    }
+
+    public void setRoleName(String roleName) {
+        this.roleName = roleName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
