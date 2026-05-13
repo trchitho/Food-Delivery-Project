1a9b9b044936e6937821836d82911bed121e3df5
Tho <tranchitho160704@gmail.com>
2026-05-13 16:18:01 +0700

    feat: add REST controllers - Login, Restaurant, Menu, Category, Order, User

---
+++ src/main/java/com/maxnguyen/fooddelivery/controller/LoginController.java
@@ -0,0 +1,50 @@
+package com.maxnguyen.fooddelivery.controller;
+
+import com.maxnguyen.fooddelivery.service.imp.LoginServiceImp;
+import com.maxnguyen.fooddelivery.payload.ResponseData;
+import com.maxnguyen.fooddelivery.payload.request.LoginRequest;
+import com.maxnguyen.fooddelivery.payload.request.SignUpRequest;
+import com.maxnguyen.fooddelivery.utils.JwtUtilsHelper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@CrossOrigin("*")
+@RestController
+@RequestMapping("/client")
+public class LoginController {
+    @Autowired
+    LoginServiceImp loginServiceImp;
+
+    @Autowired
+    JwtUtilsHelper jwtUtilsHelper;
+
+    Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @PostMapping("/login")
+    public ResponseEntity<?> signin(@RequestBody LoginRequest loginRequest){
+        ResponseData responseData = new ResponseData();
+
+        if (loginServiceImp.checkLogin(loginRequest.getUsername(), loginRequest.getPassword())){
+            String token = jwtUtilsHelper.generateToken(loginRequest.getUsername());
+
+            responseData.setData(token);
+        } else {
+            responseData.setData("");
+            responseData.setSuccessful(false);
+        }
+        return new ResponseEntity<>(responseData, HttpStatus.OK);
+    }
+
+    @PostMapping("/signup")
+    public ResponseEntity<?> signup(@RequestBody SignUpRequest signUpRequest){
+        ResponseData responseData = new ResponseData();
+
+        responseData.setData(loginServiceImp.addUser(signUpRequest));
+
+        return new ResponseEntity<>(responseData, HttpStatus.OK);
+    }
+}
---
+++ src/main/java/com/maxnguyen/fooddelivery/controller/MenuController.java
@@ -0,0 +1,47 @@
+package com.maxnguyen.fooddelivery.controller;
+
+import com.maxnguyen.fooddelivery.payload.ResponseData;
+import com.maxnguyen.fooddelivery.service.imp.FileServiceImp;
+import com.maxnguyen.fooddelivery.service.imp.MenuServiceImp;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+@CrossOrigin("*")
+@RestController
+@RequestMapping("/menu")
+public class MenuController {
+
+    @Autowired
+    MenuServiceImp menuServiceImp;
+    @Autowired
+    FileServiceImp fileServiceImp;
+    @PostMapping("")
+    public ResponseEntity<?> createMenu(
+            @RequestParam MultipartFile file,
+            @RequestParam String title,
+            @RequestParam boolean isFreeShip,
+            @RequestParam String timeShip,
+            @RequestParam double price,
+            @RequestParam int cate_Id,
+            @RequestParam int res_Id,
+            @RequestParam String description
+    ){
+        ResponseData responseData = new ResponseData();
+        boolean isSuccessful = menuServiceImp.createMenu(file, title, isFreeShip, timeShip, price, cate_Id, res_Id, description);
+        responseData.setData(isSuccessful);
+        return new ResponseEntity<>(responseData, HttpStatus.OK);
+    }
+
+    @GetMapping("/file/{filename:.+}")
+    public ResponseEntity<?> getMenuFile(@PathVariable String filename){
+
+        Resource resource = fileServiceImp.loadFile(filename);
+        return ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + resource.getFilename() + "\"").body(resource);
+    }
+}
