6cad88b590f2467ad7610f3422cf9f714c831917
Tho <tranchitho160704@gmail.com>
2026-05-13 16:17:14 +0700

    feat: add repositories, DTOs and request/response payload classes

---
+++ src/main/java/com/maxnguyen/fooddelivery/dto/MenuDto.java
@@ -0,0 +1,58 @@
+package com.maxnguyen.fooddelivery.dto;
+
+public class MenuDto {
+    private int id;
+    private String title;
+    private String image;
+    private boolean isFreeShip;
+    private String description;
+    private double price;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public boolean isFreeShip() {
+        return isFreeShip;
+    }
+
+    public void setFreeShip(boolean freeShip) {
+        isFreeShip = freeShip;
+    }
+}
